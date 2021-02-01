232b1fed9dff0fee3183f1d70d703c5092773266
kito0 <drewshah0@gmail.com>
2021-02-01 09:59:52 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useStyles } from '../css/HeaderStyles';
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	IconButton,
+	FormControlLabel,
+	FormGroup,
+	MenuItem,
+	Switch,
+} from '@material-ui/core';
+import { Menu, AccountCircle } from '@material-ui/icons';
+
+export default function Header() {
+	const classes = useStyles();
+	return <div>Ver:a</div>;
+}
---
+++ src/css/theme.js
@@ -0,0 +1,15 @@
+export default {
+	palette: {
+		primary: {
+			main: 'rgb(15, 15, 15)',
+			contrastText: '#959b9b',
+		},
+		secondary: {
+			main: '#5e3333',
+			contrastText: '#959b9b',
+		},
+		contrastThreshold: 3,
+		tonalOffset: 0.2,
+		spacing: 2,
+	},
+};
